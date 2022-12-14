1386beb2c72cf9bc11c01d49dd09edd153e86341
groupdore <group.dore@gmail.com>
2022-12-14 17:01:56 +0700

    OK

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 //calculate 100 numbers of pi
 var pi = 0;
-for (var i = 0; i < 1000000000000; i++) {
+for (var i = 0; i < 1000000; i++) {
   pi += (4 * Math.pow(-1, i)) / (2 * i + 1);
 }
 console.log(pi);
